fdf4d6a03f0cac632db2b1a635c860764001383e
rdghostmann <randalchukzwilson@gmail.com>
2025-07-08 13:13:23 +0100

    adding support

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,13 @@
 import ReferralHandler from "@/components/ReferralHandler/ReferralHandler"
 import "./globals.css"
 import { Toaster } from "sonner"
-
 import { Orbitron, Montserrat } from "next/font/google"
 import Head from "next/head";
-
-
+import Script from "next/script"; // <-- Add this import
 
 const montserrat = Montserrat({
   subsets: ["latin"],
-  weight: ["400", "500", "600", "700", "800", "900"], // Add weights as needed
+  weight: ["400", "500", "600", "700", "800", "900"],
   variable: "--font-montserrat",
   display: "swap",
 })
@@ -31,6 +29,20 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body cz-shortcut-listen="true">
         {children}
         <Toaster />
+        {/* Tawk.to Script */}
+        <Script id="tawkto-support" strategy="afterInteractive">
+          {`
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+              s1.async=true;
+              s1.src='https://embed.tawk.to/686d05cabe8a1b1910b8675f/1ivktt90m';
+              s1.charset='UTF-8';
+              s1.setAttribute('crossorigin','*');
+              s0.parentNode.insertBefore(s1,s0);
+            })();
+          `}
+        </Script>
       </body>
     </html>
   )
---
+++ components/SupportCenter/SupportCenter.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const SupportCenter = () => {
+    return (
+        <>
+            {/* <!--Start of Tawk.to Script--> */}
+            <script type="text/javascript">
+                var Tawk_API=Tawk_API||{ }, Tawk_LoadStart=new Date();
+                (function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/686d05cabe8a1b1910b8675f/1ivktt90m';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+})();
+            </script>
+            {/* <!--End of Tawk.to Script--> */}
+
+        </>
+    )
+}
+
+export default SupportCenter
