8b3b5f7406fa2b06237bce578d3b87726065b3d0
rdghostmann <randalchukzwilson@gmail.com>
2025-07-08 13:20:42 +0100

    adding support 1

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css"
 import { Toaster } from "sonner"
 import { Orbitron, Montserrat } from "next/font/google"
 import Head from "next/head";
-import Script from "next/script"; // <-- Add this import
+import SupportCenter from "@/components/SupportCenter/SupportCenter";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -29,20 +29,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body cz-shortcut-listen="true">
         {children}
         <Toaster />
-        {/* Tawk.to Script */}
-        <Script id="tawkto-support" strategy="afterInteractive">
-          {`
-            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-            (function(){
-              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-              s1.async=true;
-              s1.src='https://embed.tawk.to/686d05cabe8a1b1910b8675f/1ivktt90m';
-              s1.charset='UTF-8';
-              s1.setAttribute('crossorigin','*');
-              s0.parentNode.insertBefore(s1,s0);
-            })();
-          `}
-        </Script>
+        <SupportCenter />
       </body>
     </html>
   )
--- components/SupportCenter/SupportCenter.tsx
+++ components/SupportCenter/SupportCenter.tsx
@@ -1,24 +1,22 @@
-import React from 'react'
+"use client";
+import { useEffect } from "react";
 
 const SupportCenter = () => {
-    return (
-        <>
-            {/* <!--Start of Tawk.to Script--> */}
-            <script type="text/javascript">
-                var Tawk_API=Tawk_API||{ }, Tawk_LoadStart=new Date();
-                (function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-                s1.async=true;
-                s1.src='https://embed.tawk.to/686d05cabe8a1b1910b8675f/1ivktt90m';
-                s1.charset='UTF-8';
-                s1.setAttribute('crossorigin','*');
-                s0.parentNode.insertBefore(s1,s0);
-})();
-            </script>
-            {/* <!--End of Tawk.to Script--> */}
+  useEffect(() => {
+    // Prevent duplicate script injection
+    if (document.getElementById("tawkto-script")) return;
 
-        </>
-    )
-}
+    const s1 = document.createElement("script");
+    s1.id = "tawkto-script";
+    s1.async = true;
+    s1.src = "https://embed.tawk.to/686d05cabe8a1b1910b8675f/1ivktt90m";
+    s1.charset = "UTF-8";
+    s1.setAttribute("crossorigin", "*");
+    document.body.appendChild(s1);
+  }, []);
 
-export default SupportCenter
+  // No visible UI needed; Tawk.to widget floats bottom right by default
+  return null;
+};
+
+export default SupportCenter;
