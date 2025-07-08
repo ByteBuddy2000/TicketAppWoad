2ad2fd90eb154ec49ebde888bef49e717861ea3c
rdghostmann <randalchukzwilson@gmail.com>
2025-07-08 15:04:44 +0100

    adding support 6

--- app/celebrity/[slug]/page.tsx
+++ app/celebrity/[slug]/page.tsx
@@ -87,20 +87,20 @@ export default function CelebrityProfilePage({ params }: PageProps) {
 
             <div className=" grid grid-cols-1 gap-2 mb-6 sm:grid-cols-2 sm:gap-3">
               <div className="flex items-center justify-center">
-                {(celebrity.name === "Keanu Reeves" || celebrity.name === "Johnny Depp" || celebrity.name === "Lionel Richie") && (
+                {/* {(celebrity.name === "Keanu Reeves" || celebrity.name === "Johnny Depp" || celebrity.name === "Lionel Richie") && ( */}
                   <Link href="https://t.me/Stanleymgt" className="bg-green-100 text-green-800 px-3 py-1 rounded-full text-xs font-medium flex items-center">
                     <span className="w-2 h-2 bg-green-500 rounded-full mr-2"></span>
                     Booking Agent
                   </Link>
-                )}
+                {/* )} */}
               </div>
               <div className="flex justify-center space-x-2 col-span-1 sm:col-span-2">
-                {(celebrity.name === "Keanu Reeves" || celebrity.name === "Johnny Depp" || celebrity.name === "Lionel Richie") && (
+                {/* {(celebrity.name === "Keanu Reeves" || celebrity.name === "Johnny Depp" || celebrity.name === "Lionel Richie") && ( */}
                   <Link href="https://t.me/Stanleymgt" className="bg-green-100 text-green-800 px-3 py-1 rounded-full text-xs font-medium flex items-center">
                     <span className="w-2 h-2 bg-green-500 rounded-full mr-2"></span>
                     Manager
                   </Link>
-                )}
+                {/* )} */}
                 <Link href="https://t.me/Stanleymgt" className="bg-green-100 text-green-800 px-3 py-1 rounded-full text-xs font-medium flex items-center">
                   <span className="w-2 h-2 bg-green-500 rounded-full mr-2"></span>
                   Publicist
